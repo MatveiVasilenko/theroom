99bd2987d3b4393a039fa3e809a200c9f8419740
Mat <mat.vasilenkoo@gmail.com>
2019-02-09 12:33:15 +0200

    ff

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
   	// infinite: true,
   	speed: 600,
  	slidesToShow: 1,
- 	prevArrow: '<img class="slick-prev" src="img/arrowLeft.png" alt="">',
+ 	prevArrow: '<svg class="slick-prev" width="32px" height="32px"><use xlink:href="#arrowLeft"></use></svg>',
  	nextArrow: '<svg class="slick-next" width="32px" height="32px"><use xlink:href="#arrowRight"></use></svg>',
   	// adaptiveHeight: true
   	responsive: [
@@ -22,11 +22,33 @@ $(document).ready(function(){
     }]
   });
 
+ $('.testimonial-group').slick({
+    // autoplay: true,
+    // autoplaySpeed: 5000,
+    // dots: true,
+    // infinite: true,
+    speed: 600,
+  slidesToShow: 1,
+  prevArrow: '<svg class="slick-prev slick-prev--group" width="32px" height="32px"><use xlink:href="#arrowLeft"></use></svg>',
+  nextArrow: '<svg class="slick-next slick-next--group" width="32px" height="32px"><use xlink:href="#arrowRight"></use></svg>',
+    // adaptiveHeight: true
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {        
+      infinite: true,
+      dots: true,
+      arrows: false
+        }
+    }]
+  });
+
 $('.portfolios').slick({
   centerMode: true,
   centerPadding: '60px',
   slidesToShow: 3,
- 	nextArrow: '<svg class="slick-next" width="32px" height="32px"><use xlink:href="#arrowRight"></use></svg>',
+  prevArrow: '<svg class="slick-prev slick-prev--portfolio" width="48px" height="48px"><use xlink:href="#arrowLeft"></use></svg>',
+ 	nextArrow: '<svg class="slick-next slick-next--portfolio" width="48px" height="48px"><use xlink:href="#arrowRight"></use></svg>',
   // slidesToShow: 3,
   // slidesToScroll: 1,
   // infinite: true,
